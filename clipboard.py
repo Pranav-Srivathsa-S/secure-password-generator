927f8a2dbacbf94fe58d650a141bd291e8d1ebe5
Pranav-Srivathsa-S <68424518+Pranav-Srivathsa-S@users.noreply.github.com>
2023-05-17 21:56:30 +0530

    Update clipboard.py

--- clipboard.py
+++ clipboard.py
@@ -1,1 +1,15 @@
+# clipboard.py
+
+import platform
+import subprocess
+
+def copy_to_clipboard(text):
+    system = platform.system()
+
+    if system == "Windows":
+        subprocess.run(["clip"], input=text.strip().encode(), check=True, shell=True)
+    elif system == "Darwin":
+        subprocess.run(["pbcopy"], input=text.strip().encode(), check=True)
+    elif system == "Linux":
+        subprocess.run(["xclip", "-selection", "clipboard"], input=text.strip().encode(), check=True)
 
